a72c7e2f7818c03537aad4b4dc20fb9f987ab3e5
Ben <futureappworker@gmail.com>
2016-07-10 00:33:05 +0800

    Add webpack hot setting (#8)
    * Add webpack-dev-middleware and webpack-hot-middleware
    
    * Delete bundle.js

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
-
 var favicon = require('serve-favicon');
-var serveStatic = require('serve-static');
+
+global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(favicon(__dirname + '/dist/favicon.ico'));
-app.use(serveStatic('dist'));
+
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpack = require("webpack");
+var webpackConfig = require('./webpack.config');
+var compiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(compiler, {
+  contentBase: 'dist',
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath,
+  headers: {'Access-Control-Allow-Origin': '*'}
+}));
+
+if (__DEVELOPMENT__) {
+  app.use(require("webpack-hot-middleware")(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr'
+  }));
+}
 
 app.use(function(req, res, next) {
   res.sendFile(__dirname + '/dist/index.html');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-  entry: "./src/router.js",
+var webpack = require('webpack');
+
+var config = {
+  entry: [
+    './src/router.js'
+  ],
   output: {
-    path: __dirname + "/dist/build",
-    filename: "bundle.js"
+    path: __dirname + '/dist/build',
+    publicPath: '/build/',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [{
@@ -13,5 +18,30 @@ module.exports = {
         presets: ['es2015', 'react']
       }
     }]
-  }
+  },
+  plugins: []
 };
+
+/*
+if (!__DEVELOPMENT__) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  );
+}
+*/
+
+if (__DEVELOPMENT__) {
+  config.entry.unshift('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true');
+  config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+  config.devtool = 'source-map';
+}
+
+module.exports = config;
